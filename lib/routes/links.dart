a27794f229368f2cf13820f39f246d40005afbfd
sovla <sovla@ypet.co.kr>
2022-10-28 21:05:22 +0900

    add(RouterLinks) 라우터 링크 설정 완료

--- lib/routes/links.dart
+++ lib/routes/links.dart
@@ -1,8 +1,27 @@
 class AppLinks {
-  static const LOGIN = "/";
-  static const PRODUCTS = "/products";
-  static const FAVORITES = "/favorites";
-  static const ORDERS = "/orders";
-  static const DASHBOARD = "/dashboard";
-  static const ADMIN = "/manager";
+  static const HOME = '/';
+  static const COMMUNITY_HOME = '/products';
+  static const REVIEW_WRITE = '/review/write';
+  static const REVIEW_DETAIL = '/review/detail';
+  static const HOSPITAL_DETAIL = '/hospital/detail';
+  static const HOSPITAL_HOME = '/hospital/home';
+  static const HOSPITAL_REVIEW = '/hospital/review';
+  static const HOSPITAL_UPDATE = '/hospital/update';
+  static const PHARMACY_DETAIL = '/pharmacy/detail';
+  static const PHARMACY_HOME = '/pharmacy/home';
+  static const PHARMACY_REVIEW = '/pharmacy/review';
+  static const PHARMACY_UPDATE = '/pharmacy/update';
+  static const APP_FAQ = '/app/faq';
+  static const APP_NOTICE = '/app/notice';
+  static const APP_OPERATIONPOLICY = '/app/operationpolicy';
+  static const USER_ALARM = '/user/alarm';
+  static const USER_BOOKMARK = '/user/bookmark';
+  static const USER_SETTING = '/user/setting';
+  static const USER_UPDATE = '/user/update';
+  static const USER_ID_FIND = '/user/id/find';
+  static const USER_LOGIN = '/user/login';
+  static const USER_PW_FIND = '/user/pw/find';
+  static const USER_SINGUP = '/user/singup';
+
+  static const ADMIN = '/manager';
 }
